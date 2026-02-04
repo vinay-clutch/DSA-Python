f79724610fc536e9aa6bd508e2f8fcea22917177
Vinay B S <vinayvinay0256@gmail.com>
2026-02-04 22:18:59 +0530

    new problems

---
+++ arrays2/palindrom.py
@@ -0,0 +1,12 @@
+def checkpalindrom(s):
+  left = 0 
+  right = len(s) - 1
+  while left < right:
+    if s[left]!=s[right]:
+      return False
+    left += 1
+    right -= 1
+    
+  return True
+
+print(checkpalindrom(s = "madam"))
--- arrays2/twosum.py
+++ arrays2/twosum.py
@@ -1,9 +1,22 @@
-def twosum(arr,target):
-  seen = {}
+# def twosum(arr,target):
+#   seen = {}
   
-  for i in range(len(arr)):
-    need = target - arr[i]
-    if need in seen:
-      return[seen[need],i]
-    seen[arr[i]] = i
+#   for i in range(len(arr)):
+#     need = target - arr[i]
+#     if need in seen:
+#       return[seen[need],i]
+#     seen[arr[i]] = i
+  
+
+def towsum(arr,target):
+  left = 0
+  right = len(arr)-1
+  while left < right:
+    s = arr[left] + arr[right]
+    if s == target:
+      return [left,right]
+    elif s<target:
+      left += 1
+    else:
+      right -= 1
   
